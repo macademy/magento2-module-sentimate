4b6eaef332c9042ce789a2bf35a29fcd34bef45b
Mark Shust <mark@shust.com>
2024-01-24 11:27:09 -0500

    Update the queue consumer to call the API

--- Model/ReviewConsumer.php
+++ Model/ReviewConsumer.php
@@ -4,8 +4,23 @@ declare(strict_types=1);
 
 namespace Macademy\Sentimate\Model;
 
+use GuzzleHttp\Client as GuzzleClient;
+use Psr\Log\LoggerInterface;
+
 class ReviewConsumer
 {
+    /**
+     * Constructor for queue consumer.
+     *
+     * @param GuzzleClient $guzzleClient
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        private readonly GuzzleClient $guzzleClient,
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
     /**
      * Queue consumer process handler.
      *
@@ -15,6 +30,24 @@ class ReviewConsumer
     public function process(
         string $message,
     ): void {
-        // Code to execute on message
+        $response = $this->guzzleClient->request(
+            'POST',
+            'https://twinword-sentiment-analysis.p.rapidapi.com/analyze/',
+            [
+                'form_params' => [
+                    'text' => 'great value in its price range!'
+                ],
+                'headers' => [
+                    'X-RapidAPI-Host' => 'twinword-sentiment-analysis.p.rapidapi.com',
+                    'X-RapidAPI-Key' => 'API_KEY_GOES_HERE',
+                    'content-type' => 'application/x-www-form-urlencoded',
+                ],
+            ],
+        );
+
+        $this->logger->info('Sentiment Analysis', [
+            'Message' => $message,
+            'Response Body' => $response->getBody(),
+        ]);
     }
 }
