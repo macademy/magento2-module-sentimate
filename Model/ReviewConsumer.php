4eb61bfbe1019b3246d57afd57686bc25eac9b44
Mark Shust <mark@shust.com>
2024-01-24 19:43:55 -0500

    Catch exceptions from calling APIs

--- Model/ReviewConsumer.php
+++ Model/ReviewConsumer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Macademy\Sentimate\Model;
 
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
 use Psr\Log\LoggerInterface;
 
 class ReviewConsumer
@@ -30,24 +31,28 @@ class ReviewConsumer
     public function process(
         string $message,
     ): void {
-        $response = $this->guzzleClient->request(
-            'POST',
-            'https://twinword-sentiment-analysis.p.rapidapi.com/analyze/',
-            [
-                'form_params' => [
-                    'text' => 'great value in its price range!'
+        try {
+            $response = $this->guzzleClient->request(
+                'POST',
+                'https://twinword-sentiment-analysis.p.rapidapi.com/analyze/',
+                [
+                    'form_params' => [
+                        'text' => 'great value in its price range!'
+                    ],
+                    'headers' => [
+                        'X-RapidAPI-Host' => 'twinword-sentiment-analysis.p.rapidapi.com',
+                        'X-RapidAPI-Key' => 'API_KEY_GOES_HERE',
+                        'content-type' => 'application/x-www-form-urlencoded',
+                    ],
                 ],
-                'headers' => [
-                    'X-RapidAPI-Host' => 'twinword-sentiment-analysis.p.rapidapi.com',
-                    'X-RapidAPI-Key' => 'API_KEY_GOES_HERE',
-                    'content-type' => 'application/x-www-form-urlencoded',
-                ],
-            ],
-        );
+            );
 
-        $this->logger->info('Sentiment Analysis', [
-            'Message' => $message,
-            'Response Body' => $response->getBody(),
-        ]);
+            $this->logger->info('Sentiment Analysis', [
+                'Message' => $message,
+                'Response Body' => $response->getBody(),
+            ]);
+        } catch (GuzzleException $exception) {
+            $this->logger->error(__('Sentiment Analysis API returned an error: %1', $exception->getMessage()));
+        }
     }
 }
